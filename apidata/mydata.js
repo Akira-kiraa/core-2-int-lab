af69eba87401152b23ef66dea058aa92f281fb3f
Akira-kiraa <89808441+Akira-kiraa@users.noreply.github.com>
2023-05-04 16:17:54 -0400

    54

--- apidata/mydata.js
+++ apidata/mydata.js
@@ -1,39 +1,33 @@
+// 这里是样本数据，您需要根据您的实际数据来修改
 const data = [
-	{ beach: 'Beach 1', 'date':11, 'Enterococci Results': 10 },
-	{ beach: 'Beach 2', 'date':12, 'Enterococci Results': 30 },
-	{ beach: 'Beach 3', 'date':13, 'Enterococci Results': 20 },
-	{ beach: 'Beach 4', 'date':14, 'Enterococci Results': 0 },
-	{ beach: 'Beach 5', 'date':15, 'Enterococci Results': 0 },
-    { beach: 'Beach 6', 'date':16, 'Enterococci Results': 9.9 },
-    { beach: 'Beach 7', 'date':17, 'Enterococci Results': 90 },
-    { beach: 'Beach 8', 'date':18, 'Enterococci Results': 150 },
-    { beach: 'Beach 9', 'date':19, 'Enterococci Results': 200 },
-];
-
-const canvas = document.getElementById('myCanvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const ctx = canvas.getContext('2d');
-
-function drawCircle(x, y, size) {  
-  ctx.beginPath();
-  ctx.arc(x, y, size * 5, 0, 2 * Math.PI);
-  ctx.fillStyle = 'greenyellow';
-  ctx.fill();
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 1;
-  ctx.stroke();
-
-}
-
-data.forEach((beach, index) => {
-  const x = beach['date'] * 5 + (index * 100);
-  const y = 500 - beach['Enterococci Results'] * 2 ;
-  const size = beach['Enterococci Results'] === 0 ? 0 :
-  beach['Enterococci Results'] <= 100 ? 10 :
-  beach['Enterococci Results'] <= 1000 ? 20 :
-  30;
-
-  drawCircle(x, y, size);
-});
+    { enterococciResults: 50, date: '2022-04-01' },
+    { enterococciResults: 200, date: '2022-04-02' },
+    { enterococciResults: 800, date: '2022-04-03' },
+    { enterococciResults: 1200, date: '2022-04-04' },
+    { enterococciResults: 70, date: '2022-04-05' },
+  ];
+  
+  // 将数据按照日期从小到大排序
+  data.sort((a, b) => new Date(a.date) - new Date(b.date));
+  
+  // 获取图表容器
+  const chart = document.getElementById('chart');
+  
+  // 计算每个圆形的位置和颜色
+  data.forEach((item, index) => {
+    const circle = document.createElement('div');
+    circle.className = 'circle';
+    if (item.enterococciResults <= 100) {
+      circle.classList.add('green');
+    } else if (item.enterococciResults > 100 && item.enterococciResults <= 1000) {
+      circle.classList.add('yellow');
+    } else {
+      circle.classList.add('red');
+    }
+    const top = - item.enterococciResults * 0.4 + 500;
+    const left = 600 - (data.length - index) * 120;
+    circle.style.top = `${top}px`;
+    circle.style.left = `${left}px`;
+    chart.appendChild(circle);
+  });
+  
