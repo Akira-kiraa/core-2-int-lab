d38b9317d13248349531b8debd1e2264454c7d08
Akira-kiraa <89808441+Akira-kiraa@users.noreply.github.com>
2023-05-09 00:06:32 -0400

    5.9

--- nyc-beach-main/frontend/vite.config.ts
+++ nyc-beach-main/frontend/vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const base =
-  "https://akira-kiraa.github.io/core-2-int-lab/nyc-beach/frontend/dist/";
+  "https://akira-kiraa.github.io/core-2-int-lab/nyc-beach-main/static/";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../static",
+  },
   base,
   define: {
     BASE_URL: JSON.stringify(base),
